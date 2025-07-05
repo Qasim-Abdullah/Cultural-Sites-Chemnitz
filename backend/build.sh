b71d2ce10b7e631947417186bbbec77d730ad22b
RanaQasim <ranaqasim739@yahoo.com>
2025-07-06 01:30:16 +0200

    make changes to deploy on Render

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,11 @@
 set -o errexit
 
 echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Checking psycopg2 installation..."
+python -c "import psycopg2; print('psycopg2 imported successfully')"
 
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
